114373c6b87d7f167bc797d6332a14926848f416
itm154 <ashrulfahmi@gmail.com>
2025-12-26 15:10:52 +0800

    Add member table

--- member_directory.php
+++ member_directory.php
@@ -2,5 +2,50 @@
 include 'db_connect.php';
 include 'navbar.php';
 ?>
-<h1>Member directory</h1>
+
+<html>
+
+<h1>Member Directory</h1>
+<table>
+	<tr>
+		<th>ID</th>
+		<th>Name</th>
+		<th>Membership Tier</th>
+		<th>Status</th>
+		<th>Joined Programs</th>
+		<th>Actions</th>
+	</tr>
+	<?php
+
+	$query = file_get_contents('queries/member_info.sql');
+	$result = $conn->query($query);
+	while ($row = mysqli_fetch_assoc($result)) {
+	?>
+		<tr>
+			<td>
+				<?php echo $row['person_id'] ?>
+			</td>
+			<td>
+				<?php echo $row['person_name'] ?>
+			</td>
+			<td>
+				<?php echo $row['type_name'] ?>
+			</td>
+			<td>
+				<?php echo $row['membership_status'] ?>
+			</td>
+			<td>
+				<?php echo $row['program_count'] ?>
+			</td>
+			<td>
+				<a href="view_member.php?member_id=<?php echo $row['person_id'] ?>">View</a>
+			</td>
+		</tr>
+	<?php
+	}
+	?>
+</table>
+
+</html>
+
 <?php $conn->close(); ?>
