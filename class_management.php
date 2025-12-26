5c616ba5f1beea0de8fed11e97ec75c44821cc8f
arielmer <arielelmer7617@gmail.com>
2025-12-26 23:58:37 +0800

    Update class_management.php
    hi finally back from kampung so yeah just playing around with a few codes before actually locking in :3

--- class_management.php
+++ class_management.php
@@ -2,5 +2,39 @@
 include 'db_connect.php';
 include 'navbar.php'; 
 ?>
+
+<html>
 <h1>Class management</h1>
+
+<h1>Class Schedule</h1>
+
+<table>
+	<tr>
+		<th>Class ID</th>
+		<th>Date and Time</th>
+		<th>Class Status</th>
+	</tr>
+
+    <?php
+
+	$query = file_get_contents('queries/class.sql');
+	$result = $conn->query($query);
+	while ($row = mysqli_fetch_assoc($result)) {
+	?>
+		<tr>
+			<td>
+				<?php echo $row['class_id'] ?>
+			</td>
+			<td>
+				<?php echo $row['class_datetime'] ?>
+			</td>
+			<td>
+				<?php echo $row['class_status'] ?>
+			</td>
+		</tr>
+	<?php } ?>
+</table>
+
+</html>
+
 <?php $conn->close(); ?>
