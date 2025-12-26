5d07835307a28e1512a1bd2e0f342cead5bafbdd
angelina529 <angelinaa0529@gmail.com>
2025-12-26 15:35:03 +0800

    Add trainer update functionality

--- trainer_management.php
+++ trainer_management.php
@@ -16,7 +16,30 @@ if(isset($_GET['delete'])){
     $conn->query("DELETE FROM trainers WHERE id=$id");
 }
 
-// ---- FETCH TRAINERS ----
+// ---- UPDATE TRAINERS ----
+$edit_id = "";
+$edit_name = "";
+$edit_email = "";
+
+if (isset($_GET['edit'])) {
+    $edit_id = $_GET['edit'];
+    $edit_result = $conn->query("SELECT * FROM trainers WHERE id=$edit_id");
+    $edit_row = $edit_result->fetch_assoc();
+
+    $edit_name = $edit_row['name'];
+    $edit_email = $edit_row['email'];
+}
+
+// ---- UPDATE TRAINER ----
+if (isset($_POST['update_trainer'])) {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    $conn->query("UPDATE trainers SET name='$name', email='$email' WHERE id=$id");
+}
+
+// ---- FETCH ALL TRAINERS ----
 $result = $conn->query("SELECT * FROM trainers");
 ?>
 
@@ -24,9 +47,20 @@ $result = $conn->query("SELECT * FROM trainers");
 
 <!-- Add Trainer Form -->
 <form method="POST">
-    Name: <input type="text" name="name" required>
-    Email: <input type="email" name="email" required>
-    <input type="submit" name="add_trainer" value="Add Trainer">
+    <input type="hidden" name="id" value="<?php echo $edit_id; ?>">
+
+    Name: <input type="text" name="name" required
+           value="<?php echo $edit_name; ?>">
+
+    Email: <input type="email" name="email" required
+           value="<?php echo $edit_email; ?>">
+
+    <?php if ($edit_id) { ?>
+        <input type="submit" name="update_trainer" value="Update Trainer">
+        <a href="trainer_management.php">Cancel</a>
+    <?php } else { ?>
+        <input type="submit" name="add_trainer" value="Add Trainer">
+    <?php } ?>
 </form>
 
 <!-- Trainer List Table -->
@@ -43,8 +77,9 @@ $result = $conn->query("SELECT * FROM trainers");
         <td><?php echo $row['name']; ?></td>
         <td><?php echo $row['email']; ?></td>
         <td>
+            <a href="trainer_management.php?edit=<?php echo $row['id']; ?>">Edit</a> |
             <a href="trainer_management.php?delete=<?php echo $row['id']; ?>">Delete</a>
-            <!-- Later you can add Update link -->
+
         </td>
     </tr>
     <?php } ?>
