b17bcf5427e39798bb8f0998ecc56736917e9e08
arielmer <arielelmer7617@gmail.com>
2025-12-27 15:01:17 +0800

    trying to update, delete, add classes
    maybe needs a new branch?

--- class_management.php
+++ class_management.php
@@ -2,5 +2,50 @@
 include 'db_connect.php';
 include 'navbar.php'; 
 ?>
+
+<html>
 <h1>Class management</h1>
+
+<h1>Class Schedule</h1>
+
+<table>
+	<tr>
+		<th>Class ID</th>
+		<th>Date and Time</th>
+        <th>Trainer</th>
+		<th>Class Status</th>
+        
+	</tr>
+
+    <?php
+
+	$query = file_get_contents('queries/class.sql/trainer_program_history.sql');
+	$result = $conn->query($query);
+	while ($row = mysqli_fetch_assoc($result)) {
+	?>
+		<tr>
+			<td>
+				<?php echo $row['class_id'] ?>
+			</td>
+			<td>
+				<?php echo $row['class_datetime'] ?>
+			</td>
+			<td>
+				<?php echo $row['person_id'] ?>
+			</td>
+            <td>
+				<?php echo $row['class_status'] ?>
+			</td>
+			<td>
+				<a href="delete_class.php?class_id=<?php echo $row['class_id'] ?>" onclick="return confirm('Are you sure you want to delete this class?');">Delete</a>
+			</td>
+		</tr>
+	<?php } ?>
+
+    
+</table>
+
+</html>
+
 <?php $conn->close(); ?>
+
---
+++ delete_class.php
@@ -0,0 +1,26 @@
+<?php
+include 'db_connect.php';
+
+
+if (isset($_GET['class_id'])) {
+	$member_id = $_GET['class_id'];
+
+	$sql = "DELETE FROM Class WHERE class_id = ?";
+
+	if ($stmt = $conn->prepare($sql)) {
+		$stmt->bind_param("i", $param_id);
+		$param_id = $class_id;
+
+		if ($stmt->execute()) {
+			// Return to class_management page
+			header("location: class_management.php");
+			exit();
+		} else {
+			echo "<script>alert('Error deleting class: " . $conn->error . "');</script>";
+		}
+	}
+
+	$stmt->close();
+}
+
+$conn->close();
