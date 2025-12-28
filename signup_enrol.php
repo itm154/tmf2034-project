59bd7e4e9e83f017c4734fda824b0f460d1c4e49
bananasabah <jordanwalter900@gmail.com>
2025-12-28 17:20:13 +0800

    Load active programs from DB for enrolment

--- signup_enrol.php
+++ signup_enrol.php
@@ -4,3 +4,13 @@ include 'navbar.php';
 ?>
 <h1>Signup/Enrol</h1>
 <?php $conn->close(); ?>
+
+$programs =[];
+$sqlPrograms = "SELECT program_id, program_name, program_fee 
+                FROM programs
+                WHERE program_status = 'Active'
+                ORDER BY program_name ASC";
+$resPrograms = $conn->query($sqlPrograms);
+if ($resPrograms){
+    while ($row = $resPrograms->fetch_assoc()) $programs[] = $row;
+}
