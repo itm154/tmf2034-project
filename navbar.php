038e7aab9f673c2d44d0f84c084e6a40368bbf89
itm154 <ashrulfahmi@gmail.com>
2025-12-30 15:51:52 +0800

    Add bottom border to navbar

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <html lang="en" data-bs-theme="dark">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-<nav class="navbar navbar-expand-lg">
+<nav class="navbar navbar-expand-lg border-bottom">
 	<div class="container-fluid">
 		<a class="navbar-brand" href="index.php">FitLife Fitness Club</a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
