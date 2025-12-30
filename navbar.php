38df110c4ad4d3b5b2e335dc6fe04d496a09679a
itm154 <ashrulfahmi@gmail.com>
2025-12-30 14:40:34 +0800

    Add navbar component

--- navbar.php
+++ navbar.php
@@ -1,8 +1,37 @@
-<nav>
-	<a href="index.php">Dashboard</a>
-	<a href="member_directory.php">Member directory</a>
-	<a href="class_management.php">Class management</a>
-	<a href="signup_enrol.php">Signup/Enrol</a>
-	<a href="trainer_management.php">Trainer management</a>
-	<a href="programs_management.php">Programs management</a>
+<html lang="en" data-bs-theme="dark">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<nav class="navbar navbar-expand-lg">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="index.php">FitLife Fitness Club</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="index.php">Dashboard</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="member_directory.php">Member directory</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="class_management.php">Class management</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="signup_enrol.php">Signup/Enrol</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="trainer_management.php">Trainer management</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="programs_management.php">Programs management</a>
+				</li>
+			</ul>
+		</div>
+	</div>
 </nav>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</html>
