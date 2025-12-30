15148eb3210509d6cce0d62a2f7024a10a4ef32c
itm154 <ashrulfahmi@gmail.com>
2025-12-30 14:16:56 +0800

    Use prepare() for delete_class and delete_member

--- delete_class.php
+++ delete_class.php
@@ -1,26 +1,17 @@
 <?php
 include 'db_connect.php';
 
-
 if (isset($_GET['class_id'])) {
 	$class_id = $_GET['class_id'];
 
-	$sql = "DELETE FROM Class WHERE class_id = ?";
+	$stmt = $conn->prepare("DELETE FROM Class WHERE class_id = ?");
+	$stmt->bind_param("i", $class_id);
 
-	if ($stmt = $conn->prepare($sql)) {
-		$stmt->bind_param("i", $param_id);
-		$param_id = $class_id;
-
-		if ($stmt->execute()) {
-			// Return to class_management page
-			header("location: class_management.php");
-			exit();
-		} else {
-			echo "<script>alert('Error deleting class: " . $conn->error . "');</script>";
-		}
+	if ($stmt->execute()) {
+		header("location: class_management.php");
+	} else {
+		echo "<script> alert('Error deleting class'); window.location.href = 'class_management.php'; </script>";
 	}
-
 	$stmt->close();
 }
-
 $conn->close();
