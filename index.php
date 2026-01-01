e6ba9ea101bba931b8601c4630190f7b3187383c
Jnihere <jianichen05@gmail.com>
2026-01-01 16:43:03 +0800

    idk

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
-<?php
-include 'navbar.php';
-include 'db_connect.php';
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Fit Life System</title>
+</head>
+<body>
 
-<h1>Dashboard</h1>
+<h1>Fit Life System Board</h1>
+
+<ul>
+    <li><a href="dashboard.php">Dashboard</a></li>
+    <li><a href="member_directory.php">Member Directory</a></li>
+    <li><a href="class_management.php">Class management</a></li>
+    <li><a href="trainer_management.php">Trainer Management</a></li>
+    <li><a href="cf_attendance_report.php">Attendance Analytics</a></li>
+</ul>
+
 <?php $conn->close(); ?>
